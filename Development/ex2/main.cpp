d1f362e62b0e38dc204d9cbe2aefb522c58316ac
Sivan Schick <sivan.schick@vayyar.com>
2017-04-29 14:35:15 +0300

    debug mem leaks comment

--- Development/ex2/main.cpp
+++ Development/ex2/main.cpp
@@ -16,7 +16,7 @@ void print_error(const std::string& s)
 int main(int argc, char* argv[])
 {
 #ifdef _DEBUG
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Memory leak detection
 #endif
 	try
 	{
