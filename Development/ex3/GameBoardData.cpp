913a8150e7b7b6989098de4e0395aaab0927eceb
Sivan Schick <sivan.schick@vayyar.com>
2017-05-29 16:06:06 +0300

    Implementer charAt + removed redundant .cpp

--- Development/ex3/GameBoardData.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameBoardData.h"
-
-char GameBoardData::charAt(Coordinate c) const
-{
-	//TODO: implement
-	char a = 'a';
-	return a;
-}
--- Development/ex3/GameBoardData.h
+++ Development/ex3/GameBoardData.h
@@ -5,7 +5,7 @@ class GameBoardData : public BoardData
 {
 public:
 	GameBoardData(int player, const GameBoard& board) : _board(board), _player(player) {}
-	char charAt(Coordinate c) const override;
+	char charAt(Coordinate c) const override { return GameBoard::isPlayerShip(_player, _board(c)) ? _board(c) : ' '; };
 
 private:
 	const GameBoard& _board;
