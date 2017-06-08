d1d1174e079d59dc2b47a8ab35c1be7969cf415c
Sivan Schick <sivan.schick@vayyar.com>
2017-06-08 20:05:56 +0300

    init game board data fields

--- Development/ex3/GameBoardData.h
+++ Development/ex3/GameBoardData.h
@@ -4,7 +4,12 @@
 class GameBoardData : public BoardData
 {
 public:
-	GameBoardData(int player, const GameBoard& board) : _board(board), _player(player) {}
+	GameBoardData(int player, const GameBoard& board) : _board(board), _player(player)
+	{
+		_rows = board.rows();
+		_cols = board.cols();
+		_depth = board.depth();
+	}
 	char charAt(Coordinate c) const override { return GameBoard::isPlayerShip(_player, _board(c)) ? _board(c) : ' '; };
 
 private:
