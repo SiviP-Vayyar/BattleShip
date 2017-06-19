4c6ee7753095462756e6f93cf6c94dfe146c44fb
zmeir <zohar.meir@intel.com>
2017-06-19 18:39:50 +0300

    Implemented base case for 1d Matrix and added stub for nd Matrix groupValues. Passes compilation

--- Development/ex4/Matrix.cpp
+++ Development/ex4/Matrix.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <functional>
+
+template <size_t DIMENSIONS>
+using Coord = std::array<int, DIMENSIONS>;
 
 template<class T, size_t DIMENSIONS>
 class Matrix {
 public:
     typedef std::initializer_list<typename Matrix<T, DIMENSIONS-1>::listType> listType;
     typedef Matrix<T, DIMENSIONS-1> elemType;
+    typedef Coord<DIMENSIONS> coord;
+    typedef std::vector<coord> groupOfCoords;
 
     Matrix(const listType& initList) {
         for (auto& listElem : initList) {
             m.emplace_back(listElem);
         }
     }
+    
+    template<class Func, class G = typename std::result_of<Func(T)>::type>
+    std::map<G, std::vector<groupOfCoords>> groupValues(Func mappingFunc) {
+        std::map<G, std::vector<groupOfCoords>> groups;
+        
+        // TODO: change to real implementation - copied from 1d specialization
+        // for (int i = 0 ; i < m.size() ;) {
+        //     auto key = mappingFunc(m[i]);
+        //     groupOfCoords group;
+        //     for (; i < m.size() && m[i] == key ; i++) {
+        //         group.emplace_back(i);
+        //     }
+        //     groups[key].push_back(std::move(group));
+        // }
+        
+        return groups;
+    }
+    
 private:
     std::vector<elemType> m;    
 };
@@ -22,12 +47,31 @@ class Matrix<T, 1> {
 public:
     typedef std::initializer_list<T> listType;
     typedef T elemType;
+    typedef Coord<1> coord;
+    typedef std::vector<coord> groupOfCoords;
 
     Matrix(const listType& initList) {
         for (auto& listElem : initList) {
             m.push_back(listElem);
         }
     }
+    
+    template<class Func, class G = typename std::result_of<Func(T)>::type>
+    std::map<G, std::vector<groupOfCoords>> groupValues(Func mappingFunc) {
+        std::map<G, std::vector<groupOfCoords>> groups;
+        
+        for (int i = 0 ; i < m.size() ;) {
+            auto key = mappingFunc(m[i]);
+            groupOfCoords group;
+            for (; i < m.size() && m[i] == key ; i++) {
+                group.emplace_back(i);
+            }
+            groups[key].push_back(std::move(group));
+        }
+        
+        return groups;
+    }
+    
 private:
     std::vector<elemType> m;
 };
@@ -63,6 +107,6 @@ void print(const Groups& all_groups) {
 int main() {
     std::cout << "Starting main" << std::endl;
     Matrix2d<char> m = {{'a', 'A', 'a'}, {'B', 'a', 'B'}, {'B', 'a', 'B'}};
-    // auto all_groups = m.groupValues([](auto i){return islower(i)? "L": "U";});
-    // print(all_groups);
+    auto all_groups = m.groupValues([](auto i){return islower(i)? "L": "U";});
+    print(all_groups);
 }
